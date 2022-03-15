6698c6f4de321ba7e0bbc65906367a1e58376897
ladidol <599426945@qq.com>
2022-03-15 23:06:19 +0800

    Site updated: 2022-03-15 23:06:19

--- js/main.js
+++ js/main.js
@@ -1,4 +1,128 @@
 document.addEventListener('DOMContentLoaded', function () {
+  (function fairyDustCursor() {
+
+    var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var cursor = { x: width / 2, y: width / 2 };
+    var particles = [];
+
+    function init() {
+      bindEvents();
+      loop();
+    }
+
+    // Bind events that are needed
+    function bindEvents() {
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('touchmove', onTouchMove);
+      document.addEventListener('touchstart', onTouchMove);
+
+      window.addEventListener('resize', onWindowResize);
+    }
+
+    function onWindowResize(e) {
+      width = window.innerWidth;
+      height = window.innerHeight;
+    }
+
+    function onTouchMove(e) {
+      if (e.touches.length > 0) {
+        for (var i = 0; i < e.touches.length; i++) {
+          addParticle(e.touches[i].clientX, e.touches[i].clientY, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+        }
+      }
+    }
+
+    function onMouseMove(e) {
+      cursor.x = e.clientX;
+      cursor.y = e.clientY;
+
+      addParticle(cursor.x, cursor.y, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+    }
+
+    function addParticle(x, y, color) {
+      var particle = new Particle();
+      particle.init(x, y, color);
+      particles.push(particle);
+    }
+
+    function updateParticles() {
+
+      for (var i = 0; i < particles.length; i++) {
+        particles[i].update();
+      }
+
+      for (var i = particles.length - 1; i >= 0; i--) {
+        if (particles[i].lifeSpan < 0) {
+          particles[i].die();
+          particles.splice(i, 1);
+        }
+      }
+
+    }
+
+    function loop() {
+      requestAnimationFrame(loop);
+      updateParticles();
+    }
+
+    function Particle() {
+
+      this.character = "*";
+      this.lifeSpan = 120; //ms
+      this.initialStyles = {
+        "position": "fixed",
+        "top": "0", //必须加
+        "display": "block",
+        "pointerEvents": "none",
+        "z-index": "10000000",
+        "fontSize": "20px",
+        "will-change": "transform"
+      };
+
+      this.init = function (x, y, color) {
+
+        this.velocity = {
+          x: (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
+          y: 1
+        };
+
+        this.position = { x: x - 10, y: y - 20 };
+        this.initialStyles.color = color;
+        console.log(color);
+
+        this.element = document.createElement('span');
+        this.element.innerHTML = this.character;
+        applyProperties(this.element, this.initialStyles);
+        this.update();
+
+        document.body.appendChild(this.element);
+      };
+
+      this.update = function () {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.lifeSpan--;
+
+        this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")";
+      }
+
+      this.die = function () {
+        this.element.parentNode.removeChild(this.element);
+      }
+
+    }
+
+    function applyProperties(target, properties) {
+      for (var key in properties) {
+        target.style[key] = properties[key];
+      }
+    }
+
+    init();
+  })();
+
   let blogNameWidth, menusWidth, searchWidth, $nav
   let mobileSidebarOpen = false
 
@@ -146,7 +270,7 @@ document.addEventListener('DOMContentLoaded', function () {
       this.classList.toggle('expand-done')
     }
 
-    function createEle (lang, item, service) {
+    function createEle(lang, item, service) {
       const fragment = document.createDocumentFragment()
 
       if (isShowTool) {
@@ -205,7 +329,7 @@ document.addEventListener('DOMContentLoaded', function () {
   /**
    * PhotoFigcaption
    */
-  function addPhotoFigcaption () {
+  function addPhotoFigcaption() {
     document.querySelectorAll('#article-container img').forEach(function (item) {
       const parentEle = item.parentNode
       const altValue = item.title || item.alt
@@ -265,7 +389,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // find the scroll direction
-    function scrollDirection (currentTop) {
+    function scrollDirection(currentTop) {
       const result = currentTop > initTop // true is down & false is up
       initTop = currentTop
       return result
@@ -455,7 +579,7 @@ document.addEventListener('DOMContentLoaded', function () {
       newEle.className = 'fas fa-sign-out-alt exit-readmode'
       $body.appendChild(newEle)
 
-      function clickFn () {
+      function clickFn() {
         $body.classList.remove('read-mode')
         newEle.remove()
         newEle.removeEventListener('click', clickFn)
@@ -557,10 +681,10 @@ document.addEventListener('DOMContentLoaded', function () {
       let textFont; const copyFont = window.getSelection(0).toString()
       if (copyFont.length > copyright.limitCount) {
         textFont = copyFont + '\n' + '\n' + '\n' +
-        copyright.languages.author + '\n' +
-        copyright.languages.link + window.location.href + '\n' +
-        copyright.languages.source + '\n' +
-        copyright.languages.info
+          copyright.languages.author + '\n' +
+          copyright.languages.link + window.location.href + '\n' +
+          copyright.languages.source + '\n' +
+          copyright.languages.info
       } else {
         textFont = copyFont
       }
